6594891d053e8c694a8610842281494104fa296f
wobsoriano <sorianorobertc@gmail.com>
2022-12-01 20:40:23 -0800

    fix runtimeConfig import

--- src/runtime/server.ts
+++ src/runtime/server.ts
@@ -1,16 +1,13 @@
 import type { Server } from 'http'
 import { Server as SocketServer } from 'socket.io'
 import { eventHandler } from 'h3'
-import { useRuntimeConfig } from '#imports'
-
-const runtimeConfig = useRuntimeConfig()
 
 export function createIOHandler<T extends Record<string, (io: SocketServer) => void>> (functions: T) {
   return eventHandler((event) => {
     // @ts-ignore
     if (!event.node.req.$io) {
       const httpServer = (event.node.req.socket as any).server as Server
-      const io = new SocketServer(httpServer, runtimeConfig.serverOptions)
+      const io = new SocketServer(httpServer, useRuntimeConfig().serverOptions)
 
       Object.keys(functions).forEach((fn) => {
         functions[fn](io)
