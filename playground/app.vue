abba49026f787fa908a684c86315e75d2a265b03
wobsoriano <sorianorobertc@gmail.com>
2024-11-11 13:59:16 -0800

    update playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-const socket = useSocket()
+const { $io } = useNuxtApp()
 
 const connected = ref(false)
 
 onMounted(() => {
+  const socket = $io('http://localhost:3000')
+
   socket.on('connect', () => {
     connected.value = socket.connected
   })
